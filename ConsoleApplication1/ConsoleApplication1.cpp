80f7fa3d9280bcff8318c777fd97c47c91382f8e
2077946749 <2077946749@qq.com>
2020-04-17 15:34:32 +0800

    00

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,9 +1,8 @@
-﻿
-#include <opencv2\opencv.hpp>
+﻿#include <opencv2\opencv.hpp>
 #include <iostream>
 
-     using namespace cv;
-     using namespace std;
+using namespace cv;
+using namespace std;
 
 int main()
 {
@@ -12,8 +11,8 @@ int main()
 	cv::Mat dspMat;
 	cv::Mat binaryMat;
 
-	srcMat = imread("die_on_chip.png", 0);
-	dspMat = imread("die_on_chip.png");
+	srcMat = imread("rim.png", 0);
+	dspMat = imread("rim.png");
 
 	cv::Mat inversedMat = 255 - srcMat;
 	threshold(inversedMat, binaryMat, 150, 255, THRESH_OTSU);
@@ -26,14 +25,14 @@ int main()
 	{
 		RotatedRect rect = minAreaRect(contours[i]);
 
-		drawContours(dspMat, contours, i, Scalar(0,0,255), 1, 8);
-		Point2f P[4];
-		rect.points(P);
-		for (int j = 0; j <= 3; ++j)
-		{
-			line(dspMat, P[j], P[j < 3 ? j + 1 : 0], Scalar(0,0,255), 2, CV_AA);
-		}
+		drawContours(dspMat, contours, i, Scalar(0, 0, 255), 1, 8);
+
+		Point2f center;
+		float radius;
+		minEnclosingCircle(contours[i], center, radius);
+		circle(dspMat, center, radius, Scalar(0,0,255), 2, CV_AA);
 	}
+
 	imshow("src", srcMat);
 	imshow("dis", dspMat);
 	waitKey(0);
